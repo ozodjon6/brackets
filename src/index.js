bc319b1fe0e5f99ce3e6f9cd88ff8c794dbf4f9f
MedBul <55193557+MedBul@users.noreply.github.com>
2020-10-13 15:49:21 +0500

    init: start file pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const result = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  for (let el of str.split``) {
+    if(result[0] === el) {
+      result.shift();
+    } else if (brackets[el]) {
+      result.unshift(brackets[el]);
+    } else {
+      return false;
+    }
+  }
+
+  return result.length === 0;
+
 }
